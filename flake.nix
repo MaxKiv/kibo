2929c7b76d5c760605f748f6b2fff7f3a128b929
MaxKiv <maxkivits42@gmail.com>
2025-03-25 11:05:09 +0100

    feat(nix): add nix devshell

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "My new project";
+
+  inputs = {
+    your-nixos-flake.url = "github:maxkiv/nix";
+    nixpkgs.follows = "your-nixos-flake/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  # Outputs this flake produces
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = (import nixpkgs) {
+        inherit system;
+      };
+    in {
+      # Development shells provided by this flake, to use:
+      # nix develop .#default
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nil # nix LSP
+          alejandra # nix Formatter
+
+          kicad # PCB design software
+          freecad # 3D modelling software
+        ];
+      };
+
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    });
+}
